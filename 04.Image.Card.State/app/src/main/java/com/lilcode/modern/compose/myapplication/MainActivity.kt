41fe71775b9b0bc5d64b65b8d798a99f6375b417
sslee <sangsulee@gmarket.com>
2022-04-05 23:06:15 +0900

    Card + 좋아요 기능 + rememberSaveable

--- 04.Image.Card.State/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
+++ 04.Image.Card.State/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,7 +44,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ImageCard() {
-    var isFavorite by remember {
+    var isFavorite by rememberSaveable {
         mutableStateOf(false)
     }
 
