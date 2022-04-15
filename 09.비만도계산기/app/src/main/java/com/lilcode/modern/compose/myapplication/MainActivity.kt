7f06c4569101e5f21c06b04ec2a94c24b9ed1548
sslee <sangsulee@gmarket.com>
2022-04-16 00:43:25 +0900

    Feat: 09: bmi 계산 로직 추가 (viewModel)

--- 09.비만도계산기/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
+++ 09.비만도계산기/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
@@ -40,6 +40,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.lilcode.modern.compose.myapplication.ui.theme.MyApplicationTheme
 import kotlinx.coroutines.launch
+import kotlin.math.pow
 
 /**
  * https://www.inflearn.com/course/%EB%AA%A8%EB%8D%98-%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EC%BB%B4%ED%8F%AC%EC%A6%88/lecture/94903?tab=note&volume=1.00&quality=1080
@@ -51,29 +52,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val viewModel = viewModel<BmiViewModel>()
             val navController = rememberNavController()
+
+            val bmi = viewModel.bmi.value
 
             NavHost(
                 navController = navController,
                 startDestination = "home"
             ) {
                 composable(route = "home") {
-                    HomeScreen(navController)
+                    HomeScreen() { height, weight ->
+                        viewModel.bmiCalculate(height, weight)
+                        navController.navigate("result")
+                    }
                 }
                 composable(route = "result") {
-                    ResultScreen(bmi = 35.0, navController = navController)
+                    ResultScreen(bmi = bmi, navController = navController)
                 }
-
             }
-
-
         }
     }
 
 }
 
 @Composable
-fun HomeScreen(navController: NavController) {
+fun HomeScreen(
+    onResultClicked: (Double, Double) -> Unit
+) {
     val (height, setHeight) = rememberSaveable {
         mutableStateOf("")
     }
@@ -108,7 +114,9 @@ fun HomeScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(8.dp))
             Button(
                 onClick = {
-                    navController.navigate("result")
+                    if (height.isNotEmpty() && weight.isNotEmpty()) {
+                        onResultClicked(height.toDouble(), weight.toDouble())
+                    }
                 },
                 modifier = Modifier.align(Alignment.End)
             ) {
@@ -121,6 +129,22 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun ResultScreen(navController: NavController, bmi: Double) {
+
+    val text = when {
+        bmi >= 35 -> "고도 비만"
+        bmi >= 30 -> "2단계 비만"
+        bmi >= 25 -> "1단계 비만"
+        bmi >= 23 -> "과체중"
+        bmi >= 18.5 -> "정상"
+        else -> "저체중"
+    }
+
+    val imageRes = when {
+        bmi >= 23 -> R.drawable.ic_baseline_sentiment_very_dissatisfied_24
+        bmi >= 18.5 -> R.drawable.ic_baseline_sentiment_satisfied_24
+        else -> R.drawable.ic_baseline_sentiment_dissatisfied_24
+    }
+
 
     Scaffold(
         topBar = {
@@ -141,10 +165,10 @@ fun ResultScreen(navController: NavController, bmi: Double) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("과체중", fontSize = 30.sp)
+            Text(text, fontSize = 30.sp)
             Spacer(modifier = Modifier.height(50.dp))
             Image(
-                painter = painterResource(id = R.drawable.ic_baseline_sentiment_dissatisfied_24),
+                painter = painterResource(id = imageRes),
                 contentDescription = null,
                 modifier = Modifier.size(100.dp),
                 colorFilter = ColorFilter.tint(
@@ -154,4 +178,16 @@ fun ResultScreen(navController: NavController, bmi: Double) {
         }
     }
 
+}
+
+class BmiViewModel : ViewModel() {
+    private val _bmi = mutableStateOf<Double>(0.0)
+    val bmi: State<Double> = _bmi
+
+    fun bmiCalculate(
+        height: Double,
+        weight: Double
+    ) {
+        _bmi.value = weight / (height / 100.0).pow(2.0)
+    }
 }
