651c7cf2666db0856dc7b958fc19635066f2fa1f
sslee <sangsulee@gmarket.com>
2022-04-26 23:19:40 +0900

    Feat: 11: viewModel 정의

---
+++ 11.나만의_웹_브라우저/app/src/main/java/com/lilcode/modern/compose/myapplication/MainViewModel.kt
@@ -0,0 +1,34 @@
+package com.lilcode.modern.compose.myapplication
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    val url = mutableStateOf("https://www.google.com")
+
+    /*
+        한번에 하나의 데이터 발행, 동일한 데이터 발행
+        default 값 설정 안해도됨.
+         */
+    private val _undoSharedFlow = MutableSharedFlow<Boolean>()
+    val undoSharedFlow = _undoSharedFlow.asSharedFlow()
+
+    private val _redoSharedFlow = MutableSharedFlow<Boolean>()
+    val redoSharedFlow = _redoSharedFlow.asSharedFlow()
+
+    fun undo() {
+        viewModelScope.launch {
+            _undoSharedFlow.emit(true)
+        }
+    }
+
+    fun redo() {
+        viewModelScope.launch {
+            _redoSharedFlow.emit(true)
+        }
+    }
+}
