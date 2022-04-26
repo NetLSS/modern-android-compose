970fd276ad7b82e8573bb28a53c9e54d8badec52
sslee <sangsulee@gmarket.com>
2022-04-26 23:11:47 +0900

    Feat: 11: 기본 적인 UI 구성 구현

--- 11.나만의_웹_브라우저/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
+++ 11.나만의_웹_브라우저/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lilcode.modern.compose.myapplication
 
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.contextaware.withContextAvailable
@@ -8,11 +9,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.snapshots.SnapshotStateList
@@ -21,10 +24,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -44,8 +49,71 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-
+            HomeScreen()
         }
     }
 
+}
+
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "나만의 웹 브라우저") },
+                actions = {
+                    IconButton(onClick = {
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "back",
+                            tint = Color.White
+                        )
+                    }
+
+                    IconButton(onClick = {
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowForward,
+                            contentDescription = "forward",
+                            tint = Color.White
+                        )
+                    }
+                })
+        }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+                .fillMaxSize()
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("https://") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions(onSearch = {})
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MyWebView()
+    }
+}
+
+@Composable
+fun MyWebView() {
+    // factory 화면에 표시해야하는 뷰 객체 인스턴스
+    // update 는 컴포저블 될 때 실행됨.
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = {
+            WebView(it).apply {
+                loadUrl("https://google.com")
+            }
+        }, update = {
+
+        })
 }
