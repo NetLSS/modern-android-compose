c07fa2c35a60e63e24758d888918050ade3ca80f
sslee <sangsulee@gmarket.com>
2022-04-26 23:45:49 +0900

    Feat: 11: 기능 정의

--- 11.나만의_웹_브라우저/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
+++ 11.나만의_웹_브라우저/app/src/main/java/com/lilcode/modern/compose/myapplication/MainActivity.kt
@@ -25,6 +25,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -38,6 +40,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.concurrent.timer
 import kotlin.math.pow
@@ -51,20 +55,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            HomeScreen()
+            val viewModel = viewModel<MainViewModel>()
+            HomeScreen(viewModel)
         }
     }
 
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: MainViewModel) {
+    val focusManager = LocalFocusManager.current
+
+    val (inputUrl, setUrl) = rememberSaveable {
+        mutableStateOf("https://www.google.com")
+    }
+
+    val scaffoldState = rememberScaffoldState()
+
     Scaffold(
         topBar = {
             TopAppBar(title = { Text(text = "나만의 웹 브라우저") },
                 actions = {
                     IconButton(onClick = {
-
+                        viewModel.undo()
                     }) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
@@ -74,7 +87,7 @@ fun HomeScreen() {
                     }
 
                     IconButton(onClick = {
-
+                        viewModel.redo()
                     }) {
                         Icon(
                             imageVector = Icons.Default.ArrowForward,
@@ -83,41 +96,105 @@ fun HomeScreen() {
                         )
                     }
                 })
-        }
+        },
+        scaffoldState = scaffoldState
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
                 .fillMaxSize()
         ) {
             OutlinedTextField(
-                value = "",
-                onValueChange = {},
+                value = inputUrl,
+                onValueChange = setUrl,
                 label = { Text("https://") },
                 modifier = Modifier.fillMaxWidth(),
                 keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-                keyboardActions = KeyboardActions(onSearch = {})
+                keyboardActions = KeyboardActions(onSearch = {
+                    viewModel.url.value = inputUrl
+                    focusManager.clearFocus()
+                })
             )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            MyWebView(viewModel, scaffoldState)
         }
 
-        Spacer(modifier = Modifier.height(16.dp))
 
-        MyWebView()
     }
 }
 
 @Composable
-fun MyWebView() {
+fun MyWebView(viewModel: MainViewModel, scaffoldState: ScaffoldState) {
+
+    // 뒤로가기 앞으로가기, flow 사용을 위해 코루틴 필요.
+//    val scope = rememberCoroutineScope()
+
+    val webView = rememberWebView()
+    /*
+    key에 전달죄는 객체가 변경 되었을 떄 block 실행
+    Unit 한번 만 실행
+     */
+    LaunchedEffect(key1 = Unit) {
+        viewModel.undoSharedFlow.collectLatest {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                scaffoldState.snackbarHostState.showSnackbar("더 이상 뒤로 갈 수 없음")
+            }
+        }
+    }
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.redoSharedFlow.collectLatest {
+            if (webView.canGoForward()) {
+                webView.goForward()
+            } else {
+                scaffoldState.snackbarHostState.showSnackbar("더 이상 앞으로 갈 수 없음")
+            }
+        }
+    }
+
+
     // factory 화면에 표시해야하는 뷰 객체 인스턴스
     // update 는 컴포저블 될 때 실행됨.
     AndroidView(
         modifier = Modifier.fillMaxSize(),
-        factory = {
-            WebView(it).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl("https://google.com")
-            }
-        }, update = {
+        factory = { webView }, update = { webView ->
+            webView.loadUrl(viewModel.url.value)
 
+            // 빈 공백 으로 검색후 뒤로가기 2번 하면 반복적으로 더이상 뒤로갈 수 없다는 스낵바 가 뜨는 버그가 있음.
+//            scope.launch {
+//                viewModel.undoSharedFlow.collectLatest {
+//                    if (webView.canGoBack()) {
+//                        webView.goBack()
+//                    } else {
+//                        scaffoldState.snackbarHostState.showSnackbar("더 이상 뒤로 갈 수 없음")
+//                    }
+//                }
+//            }
+//
+//            scope.launch {
+//                viewModel.redoSharedFlow.collectLatest {
+//                    if (webView.canGoForward()) {
+//                        webView.goForward()
+//                    } else {
+//                        scaffoldState.snackbarHostState.showSnackbar("더 이상 앞으로 갈 수 없음")
+//                    }
+//                }
+//            }
         })
+}
+
+@Composable
+fun rememberWebView(): WebView {
+    val context = LocalContext.current
+    val webview = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://google.com")
+        }
+    }
+    return webview
 }
